6bd3c1470e7010f8f856abee1fab7f69fd6fcf79
Chelsea Jansen <misschelseachristine@gmail.com>
2018-11-27 18:15:22 +0000

    Adds a tip calculator to our list of python code snippets

--- hello-world.py
+++ hello-world.py
@@ -6,3 +6,16 @@ x = 5
 y = "John "
 
 print(x)
+
+#this is how you leave a comment without it effecting the programming
+
+#This is a tip calculator that asks for user input in order to calculate the
+#correct tax and tip
+
+price= float(input("What is the cost of your meal?(in the form of 00.00): "))
+tax= 6.75/100
+tip= 15.0/100
+price=price + tax*price
+total=price + tip*price
+
+print("Your total, including tax and tip, is: " , total)
